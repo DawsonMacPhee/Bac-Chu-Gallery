daac63eecb230bc9b62a590ead88bbd6c489e492
DawsonMacPhee <dawsonmacphee@icloud.com>
2021-08-01 19:44:12 -0400

    Fixed request for object view

--- custom/CaView.js
+++ custom/CaView.js
@@ -1,26 +1,3 @@
-var response = {
-    "ok": true,
-    "total": 1,
-    "results": [
-        {
-            "object_id": "3",
-            "id": "3",
-            "idno": "UG1974.034",
-            "display_label": "Schlachtfeld (Battlefield) No. 6 in the Peasant's War Series",
-            "ca_object_representations.media.original": "https://bachinski-chu.uoguelph.ca/admin/media/collectiveaccess/images/0/14440_ca_object_representations_media_32_original.jpg",
-            "ca_objects.displayEdition": "Not Editioned",
-            "ca_entities.preferred_labels.displayname": [
-                "Käthe Kollwitz"
-            ],
-            "ca_objects.displayCreationDate": "1907",
-            "ca_objects.displayMaterialsTech": "Etching",
-            "ca_objects.dimensionsPrint": "n/a; 15.75 x 20.75 in",
-            "ca_objects.provenance": "Ferdinand Roten Galleries, Baltimore; Gift of the Fine Art Printmaking Students in 1974 (Nasby 1980, 204).",
-            "ca_objects.rightsWork": "Out of Copyright"
-        }
-    ]
-}
-
 const app = Vue.createApp({
     data() {
         return {
@@ -58,7 +35,7 @@ const app = Vue.createApp({
     mounted: async function() {
         var ref = window.location.search.substring(window.location.search.indexOf('ref=') + 4);
 
-        /*var data = JSON.stringify(
+        var data = JSON.stringify(
             {
                 "bundles": {
                     "ca_object_representations.media.original":{"returnURL":true},
@@ -78,7 +55,7 @@ const app = Vue.createApp({
         xhr.setRequestHeader("Content-Type", "application/json");
         xhr.send(null);
 
-        var response = JSON.parse(xhr.responseText);*/
+        var response = JSON.parse(xhr.responseText);
 
         this.title = response.results[0]["display_label"];
         this.image = response.results[0]["ca_object_representations.media.original"];
@@ -90,6 +67,10 @@ const app = Vue.createApp({
         this.dimensions = response.results[0]["ca_objects.dimensionsPrint"];
         this.creditLine = response.results[0]["ca_objects.provenance"];
         this.rights = response.results[0]["ca_objects.rightsWork"];
+
+        if (this.image == null || this.image == "") {
+            this.image = "/custom/no_image.png";
+        }
     }
 });
 
