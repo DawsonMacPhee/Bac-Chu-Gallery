6e48b1df64c0499eabae40e27e0246b1dd6530f5
DawsonMacPhee <dawsonmacphee@icloud.com>
2021-08-09 20:46:23 -0400

    Switched home page to async load

--- custom/CaHome.js
+++ custom/CaHome.js
@@ -2,7 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             links: [],
-            images: [],
+            images: ["/custom/loading.gif", "/custom/loading.gif", "/custom/loading.gif", "/custom/loading.gif", "/custom/loading.gif", "/custom/loading.gif"],
             widths: [],
             heights: [],
             titles: [],
@@ -28,7 +28,44 @@ const app = Vue.createApp({
 
 
 
+    methods: {
+        loadPreview(responseText) {
+            var response = JSON.parse(responseText);
+
+            for (var i = 0; i < 6; i++) {
+                if (response.results[i]["ca_object_representations.media.large"] != null) {
+                    var imageInfo = response.results[i]["ca_object_representations.media.large"].split("'");
+                    this.images.push(imageInfo[1]);
+                    this.widths.push(imageInfo[3]);
+                    this.heights.push(imageInfo[5]);
+                } else {
+                    this.images.push("/custom/no_image.png");
+                    this.widths.push("1150");
+                    this.heights.push("647");
+                }
+                this.titles.push(response.results[i]["display_label"]);
+                this.years.push(response.results[i]["ca_objects.displayCreationDate"]);
+                this.mediums.push(response.results[i]["ca_objects.displayMaterialsTech"]);
+                this.ids.push(response.results[i]["idno"]);
+                this.links.push("https://bachinski-chu.uoguelph.ca/View-Object.html?ref=" + response.results[i]["id"]);
+
+                var creatorStr = "";
+                for (var y = 0; y < response.results[i]["ca_entities.preferred_labels.displayname"].length; y++) {
+                    if (y == 0) {
+                        creatorStr += response.results[i]["ca_entities.preferred_labels.displayname"][y];
+                    } else {
+                        creatorStr += ", " + response.results[i]["ca_entities.preferred_labels.displayname"][y];
+                    }
+                }
+                this.creators.push(creatorStr);
+            }
+        }
+    },
+
+
+
     mounted: async function() {
+        var _this = this;
         var data = JSON.stringify(
             {
                 "bundles": {
@@ -41,38 +78,11 @@ const app = Vue.createApp({
         );
 
         var xhr = new XMLHttpRequest();
-        xhr.open("GET", "https://public:public@bachinski-chu.uoguelph.ca/admin/service.php/find/ca_objects?q=ca_collections.collection_id:2&source=" + data, false);
+        xhr.open("GET", "https://public:public@bachinski-chu.uoguelph.ca/admin/service.php/find/ca_objects?q=ca_collections.collection_id:2&source=" + data, true);
         xhr.setRequestHeader("Content-Type", "application/json");
         xhr.send(null);
-
-        var response = JSON.parse(xhr.responseText);
-
-        for (var i = 0; i < 6; i++) {
-            if (response.results[i]["ca_object_representations.media.large"] != null) {
-                var imageInfo = response.results[i]["ca_object_representations.media.large"].split("'");
-                this.images.push(imageInfo[1]);
-                this.widths.push(imageInfo[3]);
-                this.heights.push(imageInfo[5]);
-            } else {
-                this.images.push("/custom/no_image.png");
-                this.widths.push("1150");
-                this.heights.push("647");
-            }
-            this.titles.push(response.results[i]["display_label"]);
-            this.years.push(response.results[i]["ca_objects.displayCreationDate"]);
-            this.mediums.push(response.results[i]["ca_objects.displayMaterialsTech"]);
-            this.ids.push(response.results[i]["idno"]);
-            this.links.push("https://bachinski-chu.uoguelph.ca/View-Object.html?ref=" + response.results[i]["id"]);
-
-            var creatorStr = "";
-            for (var y = 0; y < response.results[i]["ca_entities.preferred_labels.displayname"].length; y++) {
-                if (y == 0) {
-                    creatorStr += response.results[i]["ca_entities.preferred_labels.displayname"][y];
-                } else {
-                    creatorStr += ", " + response.results[i]["ca_entities.preferred_labels.displayname"][y];
-                }
-            }
-            this.creators.push(creatorStr);
+        xhr.onload = function() {
+            _this.loadPreview(this.responseText);
         }
     }
 });
