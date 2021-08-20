3ed6c23a2b4988cb70c370ec641c092a394942e9
DawsonMacPhee <dawsonmacphee@icloud.com>
2021-08-19 21:00:21 -0400

    Added moving carousel

--- custom/CaExhibit.js
+++ custom/CaExhibit.js
@@ -1,4 +1,3 @@
-
 window.addEventListener('load', function(e) {
     var pos1 = document.getElementsByClassName("carousel")[0].getBoundingClientRect();
     var pos2 = document.getElementsByClassName("carousel")[1].getBoundingClientRect();
@@ -24,3 +23,35 @@ window.addEventListener('resize', function(e) {
     document.getElementsByClassName("leftArrow")[1].style.left = (pos2.left + window.scrollX + 30) + "px";
     document.getElementsByClassName("rightArrow")[1].style.right = (pos2.right + window.scrollX - pos2.width + 30) + "px";
 });
+
+const app = Vue.createApp({
+    data() {
+        return {
+            carosel_1_page: 0,
+            carosel_1_images: ["images/ModernPandemicDisplay1.jpeg", "images/ReynoldsBuilding2.jpeg", "images/ReynoldsBuilding1.jpeg"],
+            carosel_2: []
+        }
+    },
+
+
+
+    computed: {
+        carousel_1() {
+            return this.carosel_1_images[this.carosel_1_page];
+        }
+    },
+
+
+
+    mounted: async function() {
+        window.setInterval(() => {
+            if ((this.carosel_1_page + 1) > (this.carosel_1_images.length - 1)) {
+                this.carosel_1_page = 0;
+            } else {
+                this.carosel_1_page += 1;
+            }
+        }, 5000)
+    }
+});
+
+app.mount("#ca-exhibit-app");
