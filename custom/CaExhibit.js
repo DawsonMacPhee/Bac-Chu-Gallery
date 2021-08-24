3311474e9a550c2bd271369b1b1e69d1e9986b5a
DawsonMacPhee <dawsonmacphee@icloud.com>
2021-08-24 09:58:37 -0400

    Updated arrow placement

--- custom/CaExhibit.js
+++ custom/CaExhibit.js
@@ -1,29 +1,3 @@
-window.addEventListener('load', function(e) {
-    var pos1 = document.getElementsByClassName("carousel")[0].getBoundingClientRect();
-    var pos2 = document.getElementsByClassName("carousel")[1].getBoundingClientRect();
-
-    document.getElementsByClassName("leftArrow")[0].style.top = (pos1.top + window.scrollY + 210) + "px";
-    document.getElementsByClassName("rightArrow")[0].style.top = (pos1.top + window.scrollY + 210) + "px";
-
-    document.getElementsByClassName("leftArrow")[1].style.top = (pos2.top + window.scrollY + 290) + "px";
-    document.getElementsByClassName("rightArrow")[1].style.top = (pos2.top + window.scrollY + 290) + "px";
-    document.getElementsByClassName("leftArrow")[1].style.left = (pos2.left + window.scrollX + 30) + "px";
-    document.getElementsByClassName("rightArrow")[1].style.right = (pos2.right + window.scrollX - pos2.width + 30) + "px";
-});
-
-window.addEventListener('resize', function(e) {
-    var pos1 = document.getElementsByClassName("carousel")[0].getBoundingClientRect();
-    var pos2 = document.getElementsByClassName("carousel")[1].getBoundingClientRect();
-
-    document.getElementsByClassName("leftArrow")[0].style.top = (pos1.top + window.scrollY + 210) + "px";
-    document.getElementsByClassName("rightArrow")[0].style.top = (pos1.top + window.scrollY + 210) + "px";
-
-    document.getElementsByClassName("leftArrow")[1].style.top = (pos2.top + window.scrollY + 290) + "px";
-    document.getElementsByClassName("rightArrow")[1].style.top = (pos2.top + window.scrollY + 290) + "px";
-    document.getElementsByClassName("leftArrow")[1].style.left = (pos2.left + window.scrollX + 30) + "px";
-    document.getElementsByClassName("rightArrow")[1].style.right = (pos2.right + window.scrollX - pos2.width + 30) + "px";
-});
-
 const app = Vue.createApp({
     data() {
         return {
