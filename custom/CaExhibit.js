8b153fead2d149f111e8488ee53d94edd93d2d34
DawsonMacPhee <dawsonmacphee@icloud.com>
2021-08-24 09:40:53 -0400

    Wired Exhibition Page Info

--- custom/CaExhibit.js
+++ custom/CaExhibit.js
@@ -27,6 +27,8 @@ window.addEventListener('resize', function(e) {
 const app = Vue.createApp({
     data() {
         return {
+            refid: 10,
+            exhibit_idno: "",
             carosel_1_page: 0,
             carosel_1_images: ["images/ModernPandemicDisplay1.jpeg", "images/ReynoldsBuilding2.jpeg", "images/ReynoldsBuilding1.jpeg"],
             carosel_2_page: 0,
@@ -52,7 +54,68 @@ const app = Vue.createApp({
 
 
 
+    methods: {
+        loadInfo(responseText) {
+            var response = JSON.parse(responseText);
+            this.title = response.results[0].display_label;
+            this.date = response.results[0]["ca_collections.nonpreferred_labels"][0];
+            this.introduction = response.results[0]["ca_collections.description"].substring(0, response.results[0]["ca_collections.description"].length - 1);
+            this.exhibit_idno = response.results[0].idno;
+
+            var _this = this;
+            var data = JSON.stringify(
+                {
+                    "bundles": {
+                        "ca_objects.displayCreationDate": true,
+                        "ca_objects.dimensionsPrint":true,
+                        "ca_objects.displayMaterialsTech": true,
+                        "ca_objects.inscriptions":true,
+                        "ca_object_representations.media.large":{"returnURL": true},
+                        "ca_entities.preferred_labels.displayname":{"returnAsArray":true}
+                    }
+                }
+            );
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", "https://public:public@bachinski-chu.uoguelph.ca/admin/service.php/find/ca_objects?q=ca_collections.collection_id:" + this.refid + "&source=" + data, true);
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.send(null);
+            xhr.onload = function() {
+                _this.loadWorks(this.responseText);
+            }
+        },
+        loadWorks(responseText) {
+            var response = JSON.parse(responseText);
+            for(var i = 0; i < response.results.length; i++) {
+                if (response.results[i].idno == "data." + this.exhibit_idno) {
+                    this.subtitle = response.results[i].display_label;
+                    this.writeUp = response.results[i]["ca_objects.inscriptions"];
+                }
+            }
+        }
+    },
+
+
+
     mounted: async function() {
+        this.refid = window.location.search.substring(window.location.search.indexOf('ref=') + 4);
+
+        var _this = this;
+        var data = JSON.stringify(
+            {
+                "bundles": {
+                    "ca_collections.description":true, 
+                    "ca_collections.nonpreferred_labels":{"returnAsArray":true}
+                }
+            }
+        );
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "https://public:public@bachinski-chu.uoguelph.ca/admin/service.php/find/ca_collections?q=ca_collections.id:" + this.refid + "&source=" + data, true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.send(null);
+        xhr.onload = function() {
+            _this.loadInfo(this.responseText);
+        }
+
         window.setInterval(() => {
             if ((this.carosel_1_page + 1) > (this.carosel_1_images.length - 1)) {
                 this.carosel_1_page = 0;
